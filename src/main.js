22dc1e24229ad4c6d86468742d1641ea851243fe
Spencer <sspencerotoni@gmail.com>
2020-03-06 23:27:03 -0300

    Render commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ class App{
     constructor(){
         this.repositorios = [],
         this.formEl = document.querySelector('form'),
+        this.ulEl = document.querySelector('ul')
         this.registerHandlers()
     } 
 
@@ -19,7 +20,33 @@ class App{
             html_url:"http://github.com/rocketseat"
         })
 
-        console.log(this.repositorios)
+        this.render()
+    }
+
+    render(){
+        this.ulEl.innerHTML = ''
+        this.repositorios.forEach(element =>{
+           let imgEl = document.createElement('img')
+            imgEl.setAttribute('src', element.avatar_url)
+
+            let strongEl = document.createElement('strong')
+            strongEl.appendChild(document.createTextNode(element.name))
+
+            let pEl = document.createElement('p')
+            pEl.appendChild(document.createTextNode(element.description))
+
+            let aEl = document.createElement('a')
+            aEl.setAttribute('target', '_blank')
+            aEl.appendChild(document.createTextNode('Acessar'))
+
+            let liEl = document.createElement('li')
+            liEl.appendChild(imgEl)
+            liEl.appendChild(strongEl)
+            liEl.appendChild(pEl)
+            liEl.appendChild(aEl)
+
+            this.ulEl.appendChild(liEl)
+        })
     }
 }
 
